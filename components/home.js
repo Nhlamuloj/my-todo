b86eb70d982d9eb2a2f2439d6b1669567afb45c7
nhlamuloj <nmahlabana@gmail.com>
2022-07-28 16:00:09 +0200

    updated files

--- components/home.js
+++ components/home.js
@@ -2,35 +2,18 @@
 
 import AddItem from "./addItem";
 import DisplayTransaction from "./displayTransaction";
-import React, { useEffect, useState } from "react";
-import {BudgetBalance} from './balance'
-import { arrayRemove } from "firebase/firestore";
+
 
 
 function Home (props){
 
-    const[expense, setExpense]= useState(0);
-    const[income, setIncome]= useState(0);
 
-    useEffect(()=>{
 
-        let expense= 0;
-        let income = 0;
-
-        for (let index = 0; index<props.list.length;index++){
-            if(props.list[index].transactionType =="Expense" ){
-               expense= expense+parseInt(props.list[index].amount);
-            }else
-            income= income+parseInt(props.list[index].amount)
-        }
-        setIncome(income)
-        setExpense(expense)
-    },[props.list.length])
+    
     return(
         <div className="container">
-            <BudgetBalance  expanse = {expense} income = {income}/>
-            <DisplayTransaction list={props.list}/>
             <AddItem add={props.add} />
+            <DisplayTransaction list={props.list}/>
         </div>
     )
 }
